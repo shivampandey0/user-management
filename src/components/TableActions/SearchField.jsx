e6322c3f6f21bebd26c4d2b06bdd3f9a2f8c45e6
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-06-10 20:07:39 +0530

    [feat] search component

---
+++ src/components/TableActions/SearchField.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import { SearchIcon } from '@heroicons/react/outline';
+
+export const SearchField = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <div className='relative'>
+      <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
+        <SearchIcon className='h-5 w-5 text-slate-500' />
+      </div>
+      <input
+        className='border-2 border-gray-300 w-80 pl-10 p-2.5 rounded-md '
+        type='search'
+        placeholder='Search by name, email or role'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+    </div>
+  );
+};
