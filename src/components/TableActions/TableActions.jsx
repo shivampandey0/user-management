aa4c8a19c2264c22cb4df61a18cf842c594a19c7
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-06-10 20:07:57 +0530

    [feat] Table actions

---
+++ src/components/TableActions/TableActions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '../Button/Button';
+import { SearchField } from './SearchField';
+
+export const TableActions = () => {
+  return (
+    <div className='flex gap-4 flex-col items-center md:flex-row justify-between'>
+      <SearchField />
+      <Button title='Delete Selected' />
+    </div>
+  );
+};
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { TableActions } from './TableActions/TableActions';
